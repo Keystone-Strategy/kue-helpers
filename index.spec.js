292886e97bf082c23996c2a1335da9a35cf0e0e8
sterlingw <sterling@sterlingw.com>
2018-03-19 10:32:06 -0700

    Remove sinon

--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,8 @@
-const sinon = require('sinon')
 const kue = require('kue')
 
 const FAKE_REDIS_URL = 'redis-url'
 const helpersExportFn = require('./index')
 const helpers = helpersExportFn(FAKE_REDIS_URL)
-
-let stubs = []
 
 test('it throws an error when the redisUrl param is not provided', () => {
   expect(helpersExportFn).toEqual(expect.any(Function))
@@ -15,10 +12,6 @@ test('it throws an error when the redisUrl param is not provided', () => {
 })
 
 describe('helpers', () => {
-  afterEach(() => {
-    stubs.forEach(stub => stub.restore())
-    stubs = []
-  })
   describe('.enqueueJob', () => {
     it('returns a promise', async () => {
       const result = helpers.enqueueJob('SomeJob', {})
@@ -27,14 +20,15 @@ describe('helpers', () => {
   })
   describe('.processAsyncJob', () => {
     it('calls queue.process with the given job name', () => {
-      const spy = sinon.spy()
-      const stub = sinon.stub(kue, 'createQueue').returns({ process: spy })
-      stubs.push(stub)
+      const spy = jest.fn()
+      const stub = jest.spyOn(kue, 'createQueue').mockReturnValue({ process: spy })
       const jobName = 'ProcessSomething'
 
       helpers.processAsyncJob(jobName)
 
-      expect(spy.calledWith(jobName)).toEqual(true)
+      expect(spy).toHaveBeenCalled()
+
+      stub.mockRestore()
     })
   })
 })
